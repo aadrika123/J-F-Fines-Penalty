949bd71feda1e0f3ef69d60b133e097bd4de2627
Chandankumar728 <chandankumar6299068@gmail.com>
2024-05-11 19:00:50 +0530

    for build

--- dist/assets/QrCode-290099d1.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-264a81f0.js";import{Q as i}from"./index-e71638ab.js";function a(e){return t.jsx(i,{value:e==null?void 0:e.url,size:e==null?void 0:e.size})}export{a as Q};
---
+++ dist/assets/QrCode-9a246a68.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-78655fd1.js";import{Q as i}from"./index-1c66840c.js";function a(e){return t.jsx(i,{value:e==null?void 0:e.url,size:e==null?void 0:e.size})}export{a as Q};
