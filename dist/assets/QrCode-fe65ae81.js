5890f28b045b889b79e6b58b88d1fe10cafa307b
Chandankumar728 <chandankumar6299068@gmail.com>
2024-06-11 12:25:55 +0530

    for build for jharkhandegovrnance

---
+++ dist/assets/QrCode-fe65ae81.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-ce317c0d.js";import{Q as i}from"./index-c53d3051.js";function a(e){return t.jsx(i,{value:e==null?void 0:e.url,size:e==null?void 0:e.size})}export{a as Q};
---
+++ dist/assets/useSetTitle-d1cf050d.js
@@ -0,0 +1,1 @@
+import{R as a,c as o,r}from"./index-ce317c0d.js";function n(t,e){const{settitleText:s,settitleBarVisibility:i}=a.useContext(o);r.useEffect(()=>{s(t),i(e)},[])}export{n as u};
